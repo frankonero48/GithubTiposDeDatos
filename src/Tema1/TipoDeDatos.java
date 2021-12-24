92ab74b6cb0a4a145389bb6cafad1cab7217683b
Frank48 <espinal.franklyn@gmail.com>
2021-12-24 08:45:07 +0100

    subiendo los primeros ejercicios

---
+++ src/Tema1/TipoDeDatos.java
@@ -0,0 +1,35 @@
+package Tema1;
+
+public class TipoDeDatos {
+
+    public static void main(String[] args) {
+
+        byte num1=10;
+        short num2=123;
+        int num3=10548;
+        long num4=1550648;
+
+        float num5= 1.5F;
+        double num6=1.2578924;
+
+        char puerta='B';
+        String planta= "Cuarta";
+
+        boolean rojo= true;
+        boolean abierto=false;
+
+        System.out.println(num1);
+        System.out.println(num2);
+        System.out.println(num3);
+        System.out.println(num4);
+        System.out.println(num5);
+        System.out.println(num6);
+        System.out.println("La puerta de mi casa es la latra " + puerta);
+        System.out.println("La planta de mi casa es la " + planta);
+        System.out.println("¿El color de mi coche es rojo? "+ rojo);
+        System.out.println("¿El bar de mi barrio esta abierto? " + abierto);
+
+
+    }
+
+}
